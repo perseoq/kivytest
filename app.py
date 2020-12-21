30db9e747196848b4f11fdbc65bf30abb7340f6c
perseoq <rodrigoqyt@gmail.com>
2020-12-21 08:02:49 -0600

    frist commit

---
+++ app.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+import kivy
+import kivy.uix.boxlayout 
+import kivy.uix.screenmanager
---
+++ main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from kivymd.app import MDApp
+
+class chocolate(MDApp):
+    def build(self):
+        return
+chocolate().run()
